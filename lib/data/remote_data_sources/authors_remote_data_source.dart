8bc5916c2cbd1b42cfce11cc4a899400021e6e72
Kuba Juziuk <kubajozok10@gmail.com>
2022-10-15 19:34:39 +0200

    adding retrofit

--- lib/data/remote_data_sources/authors_remote_data_source.dart
+++ lib/data/remote_data_sources/authors_remote_data_source.dart
@@ -1,32 +1,28 @@
 import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:user_articles/domain/models/author_model.dart';
 
-class AuthorsMockedDataSource {
-  Future<List<Map<String, dynamic>>?> getAuthors() async {
-    return [
-      {
-        'id': 1,
-        'picture': 'https://randomuser.me/api/portraits/men/57.jpg',
-        'first_name': 'Piotr',
-        'last_name': 'Obdarowicz',
-      },
-      {
-        'id': 2,
-        'picture': 'https://randomuser.me/api/portraits/men/58.jpg',
-        'first_name': 'Radosław',
-        'last_name': 'Gdański',
-      },
-    ];
-  }
+part 'authors_remote_data_source.g.dart';
+
+@RestApi(
+    baseUrl:
+        "https://my-json-server.typicode.com/adamsmaka/json-demo/json-demo")
+abstract class AuthorsRemoteRetroFitDataSource {
+  factory AuthorsRemoteRetroFitDataSource(Dio dio, {String baseUrl}) =
+      _AuthorsRemoteRetroFitDataSource;
+
+  @GET("/users")
+  Future<List<AuthorModel>> getAuthors();
 }
 
-class AuthorsRemoteDataSource {
-  Future<List<Map<String, dynamic>>?> getAuthors() async {
-    final response = await Dio().get<List<dynamic>>(
-        'https://my-json-server.typicode.com/adamsmaka/json-demo/users');
-    final listDynamic = response.data;
-    if (listDynamic == null) {
-      return null;
-    }
-    return listDynamic.map((e) => e as Map<String, dynamic>).toList();
-  }
-}
+//class AuthorsRemoteDataSource {
+  //Future<List<Map<String, dynamic>>?> getAuthors() async {
+   // final response = await Dio().get<List<dynamic>>(
+        //'https://my-json-server.typicode.com/adamsmaka/json-demo/users');
+    //final listDynamic = response.data;
+    //if (listDynamic == null) {
+      //return null;
+   // }
+    //return listDynamic.map((e) => e as Map<String, dynamic>).toList();
+ // }
+//}
