0a9b24dc8fc3e0eb1fcbc34e70b35c0234e57e56
Kuba Juziuk <kubajozok10@gmail.com>
2022-10-15 23:19:27 +0200

    getit

--- lib/features/home/page/home_page.dart
+++ lib/features/home/page/home_page.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:user_articles/app/core/enums.dart';
+import 'package:user_articles/app/injection_container.dart';
 import 'package:user_articles/data/remote_data_sources/authors_remote_data_source.dart';
 import 'package:user_articles/domain/models/author_model.dart';
 import 'package:user_articles/domain/repositories/authors_repository.dart';
@@ -18,11 +19,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       body: BlocProvider<HomeCubit>(
         create: (context) {
-          return HomeCubit(
-            authorsRepository: AuthorsRepository(
-              remoteDataSource: AuthorsRemoteRetroFitDataSource(Dio()),
-            ),
-          )..start();
+          return getIt()..start();
         },
         child: BlocBuilder<HomeCubit, HomeState>(
           builder: (context, state) {
